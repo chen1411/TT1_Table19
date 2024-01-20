b214f3838024bfa99ee585fe84aba5bf1a4a59d3
peilinnn_c99 <chen1411@e.ntu.edu.sg>
2024-01-20 10:20:17 +0800

    rough user authentication setup

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -3,12 +3,14 @@ from flask_smorest import Api
 from db import db
 from flask_login import UserMixin
 from flask_jwt_extended import JWTManager
+from flask_migrate import Migrate
 import os
 from dotenv import load_dotenv
+from blocklist import BLOCKLIST
 
 import models
 
-
+from apis.users import blp as UserBlueprint
 
 app = Flask(__name__)
 load_dotenv()
@@ -20,7 +22,60 @@ app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DB_URL")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 #app.config["PROPAGATE_EXCEPTIONS"] = True
 db.init_app(app)
+migrate = Migrate(app, db)
 api = Api(app)
 
+app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET_KEY")
+jwt = JWTManager(app)
 
+@jwt.token_in_blocklist_loader
+def check_if_token_in_blocklist(jwt_header, jwt_payload):
+    return jwt_payload["jti"] in BLOCKLIST
+
+@jwt.expired_token_loader
+def expired_token_callback(jwt_header, jwt_payload):
+    return (
+        jsonify({"message": "The token has expired.", "error": "token_expired"}),
+        401,
+    )
+
+@jwt.invalid_token_loader
+def invalid_token_callback(error):
+    return (
+        jsonify(
+            {"message": "Signature verification failed.", "error": "invalid_token"}
+        ),
+        401,
+    )
+
+@jwt.unauthorized_loader
+def missing_token_callback(error):
+    return (
+        jsonify(
+            {
+                "description": "Request does not contain an access token.",
+                "error": "authorization_required",
+            }
+        ),
+        401,
+    )
+
+@jwt.needs_fresh_token_loader
+def token_not_fresh_callback(jwt_header, jwt_payload):
+    return (
+        jsonify(
+            {
+                "description": "The token is not fresh.",
+                "error": "fresh_token_required",
+            }
+        ),
+        401,
+    )
+
+# print('check')
+# with app.app_context():
+#     print('executed')
+#     db.create_all()
+
+api.register_blueprint(UserBlueprint)
 
