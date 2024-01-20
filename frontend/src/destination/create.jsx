23e6c362aeab91c6581a5567a4b552a99428e65f
boonhaii <boonzxc@gmail.com>
2024-01-20 13:45:13 +0800

    Create destination create/edit pages

--- frontend/src/destination/create.jsx
+++ frontend/src/destination/create.jsx
@@ -1,6 +1,5 @@
 import React, {useState, useEffect} from 'react';
-import { Form, Row, Button, Alert } from 'react-bootstrap';
-
+import { Form, Row, Button, Alert, Col, Container } from 'react-bootstrap';
 
 const DestinationCreate = () => {
   const [destination, setDestination] = useState({});
@@ -41,32 +40,35 @@ const DestinationCreate = () => {
   }
 
   return (
-  <>
-    {alert.message && <Alert key={alert.type} variant={alert.type}>{alert.message}</Alert>}
-    <Form onSubmit={handleSubmit}>
-      <Row className='mb-3' rows={3}>
-        <Form.Label>Destination Name</Form.Label>
-        <Form.Control required type='text' placeholder='Enter Destination Name' onChange={handleInputChange} id='name'/>
-      </Row>
-      <Row className='mb-3' rows={3}>
-        <Form.Label>Country</Form.Label>
-        <Form.Select placeholder="Select a country" onChange={handleInputChange} id='countryId'>
-          {countries.map((country) => {
-            return (<option value={country.id}>{country.name}</option>)
-          })}
-        </Form.Select>
-      </Row>
-      <Row className='mb-3' rows={3}>
-        <Form.Label>Destination Cost</Form.Label>
-        <Form.Control required type='number' placeholder='Enter Destination Cost' onChange={handleInputChange} id='cost'/>
-      </Row>
-      <Row className='mb-3'>
-        <Form.Label>Destination Notes</Form.Label>
-        <Form.Control as='textarea' placeholder='Enter Destination Notes' onChange={handleInputChange} id='notes'/>
-      </Row>
-      <Button type="submit">Create new destination</Button>
-    </Form>
-  </>
+    <Container>
+      {alert.message && <Alert key={alert.type} variant={alert.type}>{alert.message}</Alert>}
+      <h1 className='mt-1'> Create a new destination </h1>
+      <Form onSubmit={handleSubmit}>
+        <Row className='mb-3' rows={3}>
+          <Form.Label>Destination Name</Form.Label>
+          <Form.Control required type='text' placeholder='Enter Destination Name' onChange={handleInputChange} id='name' value={destination.name}/>
+        </Row>
+        <Row className='mb-3' rows={3}>
+          <Form.Label>Country</Form.Label>
+          <Form.Select placeholder="Select a country" onChange={handleInputChange} id='countryId' value={destination.countryId}>
+            {countries.map((country) => {
+              return (<option value={country.id}>{country.name}</option>)
+            })}
+          </Form.Select>
+        </Row>
+        <Row className='mb-3' rows={3}>
+          <Form.Label>Destination Cost</Form.Label>
+          <Form.Control required type='number' placeholder='Enter Destination Cost' onChange={handleInputChange} id='cost' value={destination.cost}/>
+        </Row>
+        <Row className='mb-3'>
+          <Form.Label>Destination Notes</Form.Label>
+          <Form.Control as='textarea' placeholder='Enter Destination Notes' onChange={handleInputChange} id='notes' value={destination.notes}/>
+        </Row>
+        <Col>
+          <Button type="submit" variant="primary">Create Destination</Button>
+        </Col>
+      </Form>
+    </Container>
   )
 }
 
