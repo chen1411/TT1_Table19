2148bd3813b0ee9e5ac97772921cf701b63b7e40
Tse Hwee <87576084+hweezz@users.noreply.github.com>
2024-01-20 14:24:12 +0800

    updated styling

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -18,43 +18,69 @@ function Home() {
         title: "holiday",
         country_name: "singapore",
         budget: 1000,
-        destinations: [{ name: "uss", notes: "theme park", cost: 70 }],
+        destinations: [
+          { id: 1, name: "uss", notes: "theme park", cost: 70 },
+          { id: 2, name: "garden by the bay", notes: "park", cost: 0 },
+        ],
       },
       {
         id: 2,
         title: "grad trip",
         country_name: "south korea",
         budget: 2000,
-        destinations: [{ name: "lotte world", notes: "theme park", cost: 50 }],
+        destinations: [
+          { id: 1, name: "lotte world", notes: "theme park", cost: 50 },
+        ],
       },
     ]);
   }, []);
 
+  const deleteItinerary = async (id) => {
+    console.log(id);
+    // call delete  itinerary endpoint, input: user id from local storage
+    // await fetch(`http://localhost:5000/tasks/${id}`, { method: "DELETE" });
+
+    // setItineraries(itineraries.filter((itinerary) => itinerary.id !== id));
+  };
+
   return (
-    <div>
+    // style={{position: 'relative', top: '75px'}}
+    <div style={{backgroundColor: "black"}} > 
       {/* {userData.user 
             ? ( */}
-      <>
-        <h1>My Itineraries</h1>
+      <div className="w-75 mx-auto">
+        <Stack direction="horizontal" gap={0}>
+          <h1 className="text-light">My Itineraries</h1>
+          <h1 className="ms-auto">
+            <Link to="/signup">
+              <i className="bi bi-plus-circle text-white"></i>
+            </Link>
+          </h1>
+        </Stack>
+
         <Accordion defaultActiveKey="0">
           {itineraries.map((itinerary) => (
             <Accordion.Item eventKey={itinerary.id}>
               <Accordion.Header>
                 <Stack gap={0}>
-                  <div className="pe-2 pb-2">
-                    <Stack direction="horizontal" gap={0}>
-                      {/* increase font size for this chunk */}
-                      <b className="">{itinerary.title}</b>
-                      {/* change colours */}
-                      <div className="ms-auto pe-2">
-                        <i class="bi bi-pencil-square"></i>
-                      </div>
-                      <div className="">
-                        <i class="bi bi-trash"></i>
-                      </div>
+                  <div className="pe-3 pb-2">
+                    <Stack direction="horizontal" gap={0} >
+                      <h3>{itinerary.title}</h3>
+                      {/* change colours and update link */}
+                      <h3 className="ms-auto pe-3">
+                        <Link to="/login">
+                          <i className="bi bi-pencil-square text-secondary"></i>
+                        </Link>
+                      </h3>
+                      <h3>
+                        <i
+                          className="bi bi-trash text-danger"
+                          onClick={() => deleteItinerary(itinerary.id)}
+                        ></i>
+                      </h3>
                     </Stack>
                   </div>
-                  <div className="pe-2">
+                  <div className="pe-3">
                     <Stack direction="horizontal" gap={0}>
                       <div className="">{itinerary.country_name}</div>
                       <div className="ms-auto">Budget: ${itinerary.budget}</div>
@@ -65,18 +91,34 @@ function Home() {
               <Accordion.Body>
                 {/* margin bottom doesnt work */}
                 <b className="mb-3">Destinations:</b>
-                <ListGroup>
-                {itinerary.destinations.map((destination) => (
-                  <ListGroup.Item>{destination.name}</ListGroup.Item>
-                ))}
-                </ListGroup>
+                <ul>
+                  {itinerary.destinations.map((destination) => (
+                    <>
+                      <li key={destination.id}>
+                        <Stack direction="horizontal" gap={0}>
+                          <div className="">{destination.name}</div>
+                          <div className="ms-auto">
+                            Cost: ${destination.cost}
+                          </div>
+                        </Stack>
+                      </li>
+                      <span>{destination.notes}</span>
+                    </>
+                  ))}
+                </ul>
+                {/* <ListGroup>
+                  {itinerary.destinations.map((destination) => (
+                    <ListGroup.Item key={destination.id}><li>{destination.name}</li>
+                    <p>{destination.notes}</p></ListGroup.Item>
+                  ))}
+                </ListGroup> */}
               </Accordion.Body>
             </Accordion.Item>
           ))}
         </Accordion>
 
         {/* <Feed /> */}
-      </>
+      </div>
       {/* ) 
             : (
                 <>
