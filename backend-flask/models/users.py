2d19c6ff1b73cec81b39088f5d242140e1480a24
Lau Li Qing <63573601+liqing-lau@users.noreply.github.com>
2024-01-20 10:31:27 +0800

    update user model

--- backend-flask/models/users.py
+++ backend-flask/models/users.py
@@ -5,11 +5,7 @@ class UserModel(db.Model, UserMixin):
     __tablename__ = "users"
 
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=False, nullable=False)
-    password = db.Column(db.String(100), unique=False, nullable=False)
-    age = db.Column(db.Integer)
-    dob = db.Column(db.String(80))
-    address = db.Column(db.String(200))
-
-
-
+    username = db.Column(db.String(20), unique=False, nullable=False)
+    password = db.Column(db.String(20), unique=False, nullable=False)
+    firstname = db.Column(db.String(50), unique=False, nullable=False)
+    lastname = db.Column(db.String(50), unique=False, nullable=False)
