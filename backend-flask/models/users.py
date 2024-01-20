1b2ba70b8dca08397afaea9f71ccd5a7305f65f1
Lau Li Qing <63573601+liqing-lau@users.noreply.github.com>
2024-01-20 11:05:00 +0800

    reverted  user model

--- backend-flask/models/users.py
+++ backend-flask/models/users.py
@@ -4,8 +4,12 @@ from flask_login import UserMixin
 class UserModel(db.Model, UserMixin):
     __tablename__ = "users"
 
-    id = db.Column(db.Integer, nullable=False, autoincrement=True)
-    username = db.Column(db.String(20), unique=False, nullable=False)
-    password = db.Column(db.String(20), unique=False, nullable=False)
-    firstname = db.Column(db.String(50), unique=False, nullable=False)
-    lastname = db.Column(db.String(50), unique=False, nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=False, nullable=False)
+    password = db.Column(db.String(100), unique=False, nullable=False)
+    age = db.Column(db.Integer)
+    dob = db.Column(db.String(80))
+    address = db.Column(db.String(200))
+
+
+
